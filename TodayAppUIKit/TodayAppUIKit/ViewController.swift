1e4e7ed3aa7214b6c947f95b8f0139b2b0634844
Thomas Cowern <thomascowern@thomass-air.attlocal.net>
2022-08-22 10:43:08 -0400

    initial commit today app uikit

---
+++ TodayAppUIKit/TodayAppUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodayAppUIKit
+//
+//  Created by Thomas Cowern on 8/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
