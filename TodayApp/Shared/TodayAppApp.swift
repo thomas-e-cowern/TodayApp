8f3d60842cd6db1901ebe0c3737df389e7ed177d
Thomas Cowern <thomascowern@thomass-air.attlocal.net>
2022-08-22 10:26:23 -0400

    today app initial commit

---
+++ TodayApp/Shared/TodayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayAppApp.swift
+//  Shared
+//
+//  Created by Thomas Cowern on 8/22/22.
+//
+
+import SwiftUI
+
+@main
+struct TodayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
