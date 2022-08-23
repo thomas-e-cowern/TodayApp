2d723e96d4e032d4f19a1a54f28634341bb9e274
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-08-23 13:59:19 -0400

    updated journal detail view controller

--- TodayAppUIKit/TodayAppUIKit/JournalDetailViewController.swift
+++ TodayAppUIKit/TodayAppUIKit/JournalDetailViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class JournalDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
