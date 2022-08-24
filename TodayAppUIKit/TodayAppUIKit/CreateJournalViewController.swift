0a8a88b6c47ae86a103f81488846cf4c7f0a16ef
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-08-24 15:28:19 -0400

    updated navigation bar color and style in create journal view

--- TodayAppUIKit/TodayAppUIKit/CreateJournalViewController.swift
+++ TodayAppUIKit/TodayAppUIKit/CreateJournalViewController.swift
@@ -19,7 +19,10 @@ class CreateJournalViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        navigationBar.barTintColor = UIColor(red: 0.298, green: 0.757, blue: 0.988, alpha: 1.0)
+        navigationBar.tintColor = .white
+        navigationBar.isTranslucent = false
+        navigationBar.titleTextAttributes = [.foregroundColor : UIColor.white]
     }
     
     @IBAction func cancelButtonTapped(_ sender: Any) {
@@ -41,15 +44,4 @@ class CreateJournalViewController: UIViewController {
     @IBAction func cameraButtonTapped(_ sender: Any) {
         
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
