6cd7b43a5088af761a72fddc101fed5a3a8e2efe
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-08-26 15:04:46 -0400

    added image to url string method to picture object in models

--- TodayAppUIKit/TodayAppUIKit/Models.swift
+++ TodayAppUIKit/TodayAppUIKit/Models.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import RealmSwift
+import UIKit
 
 class Entry : Object {
     @Persisted var text = ""
@@ -18,6 +19,23 @@ class Picture : Object {
     @Persisted var fullImageName = ""
     @Persisted var thumbnailName = ""
     @Persisted var entry : Entry
+    
+    convenience init(image: UIImage) {
+        self.init()
+        fullImageName = imageToUrlString(image: image)
+    }
+    
+    func imageToUrlString(image: UIImage) -> String {
+        if let imageData = image.pngData() {
+            let fileName = UUID().uuidString + ".png"
+            if var path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+                path.appendPathComponent(fileName)
+                try? imageData.write(to: path)
+                return fileName
+            }
+        }
+        return ""
+    }
 }
 
 
