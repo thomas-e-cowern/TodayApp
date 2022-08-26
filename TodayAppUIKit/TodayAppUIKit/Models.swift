f34c5175671e82885da02bacad59020182434a82
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-08-26 15:51:24 -0400

    added scale preserving aspect ration to models file

--- TodayAppUIKit/TodayAppUIKit/Models.swift
+++ TodayAppUIKit/TodayAppUIKit/Models.swift
@@ -23,7 +23,9 @@ class Picture : Object {
     convenience init(image: UIImage) {
         self.init()
         fullImageName = imageToUrlString(image: image)
-        
+        let targetSize = CGSize(width: 500, height: 500)
+        let scaledImageSize = image.scalePreservingAspectRatio(targetSize: targetSize)
+        thumbnailName = imageToUrlString(image: scaledImageSize)
     }
     
     func imageToUrlString(image: UIImage) -> String {
@@ -39,4 +41,33 @@ class Picture : Object {
     }
 }
 
+extension UIImage {
+    func scalePreservingAspectRatio(targetSize: CGSize) -> UIImage {
+        // Determine the scale factor that preserves aspect ratio
+        let widthRatio = targetSize.width / size.width
+        let heightRatio = targetSize.height / size.height
+        
+        let scaleFactor = min(widthRatio, heightRatio)
+        
+        // Compute the new image size that preserves aspect ratio
+        let scaledImageSize = CGSize(
+            width: size.width * scaleFactor,
+            height: size.height * scaleFactor
+        )
+
+        // Draw and return the resized UIImage
+        let renderer = UIGraphicsImageRenderer(
+            size: scaledImageSize
+        )
+
+        let scaledImage = renderer.image { _ in
+            self.draw(in: CGRect(
+                origin: .zero,
+                size: scaledImageSize
+            ))
+        }
+        
+        return scaledImage
+    }
+}
 
