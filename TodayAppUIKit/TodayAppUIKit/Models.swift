a45b0b42584fcf87f378f38c4c56a94329ba7934
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-08-26 14:48:50 -0400

    added basic entry and picture class to models

--- TodayAppUIKit/TodayAppUIKit/Models.swift
+++ TodayAppUIKit/TodayAppUIKit/Models.swift
@@ -6,5 +6,18 @@
 //
 
 import Foundation
+import RealmSwift
+
+class Entry : Object {
+    @Persisted var text = ""
+    @Persisted var date = Date()
+    let pictures = List<Picture>()
+}
+
+class Picture : Object {
+    @Persisted var fullImageName = ""
+    @Persisted var thumbnailName = ""
+    @Persisted var entry : Entry
+}
 
 
