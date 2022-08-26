9a06514a03774ff6cfa6e72ba2e4c3c068ec7658
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-08-26 15:43:05 -0400

    removed toucan garbadge from the project

--- TodayAppUIKit/TodayAppUIKit/Models.swift
+++ TodayAppUIKit/TodayAppUIKit/Models.swift
@@ -23,6 +23,7 @@ class Picture : Object {
     convenience init(image: UIImage) {
         self.init()
         fullImageName = imageToUrlString(image: image)
+        
     }
     
     func imageToUrlString(image: UIImage) -> String {
